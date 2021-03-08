c38d146fe5648d3ca74140871e1c1a026eceef18
zchesny <z.chesny@gmail.com>
2021-03-07 22:56:19 -0600

    functional user cards

---
+++ src/index.js
@@ -0,0 +1,48 @@
+console.log("testing...")
+
+
+// test that we can get data from the backend
+// const BACKEND_URL = 'http://localhost:3000';
+// fetch(`${BACKEND_URL}/test`)
+//   .then(response => response.json())
+//   .then(parsedResponse => console.log(parsedResponse));
+
+  // first real data 
+const BASE_URL = "http://localhost:3000"
+const USERS_URL = `${BASE_URL}/users`
+const WORKOUTS_URL = `${BASE_URL}/workouts`
+const TEAMS_URL = `${BASE_URL}/teams`
+document.addEventListener("DOMContentLoaded", () => {
+    loadUsers(); 
+})
+
+function loadUsers() {
+    fetch(USERS_URL)
+    .then(resp => resp.json())
+    .then(json => renderCards(json)); 
+}
+
+function renderCards(json) {
+    json.forEach(user => {
+        renderCard(user); 
+    })
+}
+
+function renderCard(user) {
+    const main = document.getElementsByTagName('main')[0]; 
+    const user_id = user.id; 
+
+    const card = document.createElement('div'); 
+    card.className = 'card'; 
+    card.dataset.id = user_id; 
+
+    const userName = document.createElement('p'); 
+    userName.innerText = user.name; 
+
+    const viewWosBtn = document.createElement('button'); 
+    viewWosBtn.dataset.id = user_id; 
+    viewWosBtn.innerText = 'View Workouts'; 
+
+    main.appendChild(card); 
+    card.appendChild(userName); 
+}
